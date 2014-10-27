ded938e6b3bf3725d8110f029f1d8f0646045950
William Rennie <will.rennie@salesagility.com>
2014-10-27 14:01:18 +0000

    Latest SuiteCRM Outloog plug-in build version 1.2.0 with fixes to documents sync.

---
+++ README.md
@@ -0,0 +1,3 @@
+SuiteCRM Outlook Plugin v1.2.0
+
+For release notes and documentation on the plugin, please visit suitecrm.com
--- SuiteCRMClient/Properties/Settings.Designer.cs
+++ SuiteCRMClient/Properties/Settings.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace SuiteCRMAddIn.Properties {
+namespace SuiteCRMClient.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
