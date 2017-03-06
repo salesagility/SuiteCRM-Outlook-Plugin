404c3938f3a1603209e7176990713e704b9a4588
Simon Brooke <simon@journeyman.cc>
2017-03-06 14:07:15 +0000

    #82: Set MaximumSize to 0,0

--- SuiteCRMAddIn/frmArchive.Designer.cs
+++ SuiteCRMAddIn/frmArchive.Designer.cs
@@ -184,6 +184,7 @@ namespace SuiteCRMAddIn
             this.AcceptButton = this.btnArchive;
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
             this.AutoSize = true;
             this.CancelButton = this.btnCancel;
             this.ClientSize = new System.Drawing.Size(465, 366);
@@ -196,7 +197,6 @@ namespace SuiteCRMAddIn
             this.Controls.Add(this.txtSearch);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
-            this.MaximumSize = new System.Drawing.Size(481, 404);
             this.MinimizeBox = false;
             this.MinimumSize = new System.Drawing.Size(481, 404);
             this.Name = "frmArchive";
