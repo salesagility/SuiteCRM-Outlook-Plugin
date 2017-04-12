734195a0831bca2e062f2943b55973437da5f4c0
Simon Brooke <simon@journeyman.cc>
2017-04-12 14:49:54 +0100

    Removed ListViewEx package... again?
    Pretty sure I'd done this before.

--- ListViewEx/ListViewEx/SubItemEndEditingEventHandler.cs
+++
@@ -1,7 +0,0 @@
-namespace ListViewEx
-{
-    using System;
-    using System.Runtime.CompilerServices;
-
-    public delegate void SubItemEndEditingEventHandler(object sender, SubItemEndEditingEventArgs e);
-}
