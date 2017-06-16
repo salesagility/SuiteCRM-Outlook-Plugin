9e5d119abc802916d72e604542ec2bd935695fba
Simon Brooke <simon@journeyman.cc>
2017-06-16 12:50:41 +0100

    Upgrade to 3.0.5.2; added documentation for extensions.

---
+++ SuiteCRMAddIn/Extensions/Documentation.cs
@@ -0,0 +1,7 @@
+﻿/// <summary>
+/// The extension package contains classes whose sole purpose is to provide extension methods for other classes, 
+/// where those classes are closed source or otherwise not modifiable by us.
+/// </summary>
+namespace SuiteCRMAddIn.Extensions
+{
+}
