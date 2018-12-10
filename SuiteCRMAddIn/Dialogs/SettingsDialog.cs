5429c4ad8d5286877a80d53acdcb9ad165d8d292
Simon Brooke <simon@journeyman.cc>
2018-12-10 13:05:42 +0000

    #4635 Build 3.0.16.38, using .Net 4.6.1

--- SuiteCRMAddIn/Dialogs/SettingsDialog.cs
+++ SuiteCRMAddIn/Dialogs/SettingsDialog.cs
@@ -24,6 +24,7 @@ namespace SuiteCRMAddIn.Dialogs
 {
     using BusinessLogic;
     using Microsoft.Office.Interop.Outlook;
+    using Microsoft.Win32;
     using SuiteCRMClient;
     using SuiteCRMClient.Logging;
     using System;
@@ -95,6 +96,7 @@ namespace SuiteCRMAddIn.Dialogs
                 {
                     AddInTitleLabel.Text = ThisAddIn.AddInTitle;
                     AddInVersionLabel.Text = "Version " + ThisAddIn.AddInVersion;
+                    dotNetVersionLabel.Text = ".Net version " + CheckFor45DotVersion(Get45or451FromRegistry());
 
                     if (Globals.ThisAddIn.SuiteCRMUserSession == null)
                     {
@@ -113,6 +115,77 @@ namespace SuiteCRMAddIn.Dialogs
                     // Swallow exception!
                 }
             }
+        }
+
+        private static int Get45or451FromRegistry()
+        {
+            int result;
+            try
+            {
+                using (RegistryKey ndpKey = RegistryKey.OpenBaseKey(RegistryHive.LocalMachine, RegistryView.Registry32)
+                    .OpenSubKey("SOFTWARE\\Microsoft\\NET Framework Setup\\NDP\\v4\\Full\\"))
+                {
+                    result = Convert.ToInt32(ndpKey.GetValue("Release"));
+                }
+            }
+            catch (Exception)
+            {
+                result = 0;
+            }
+
+            return result;
+        }
+
+
+
+// Checking the version using >= will enable forward compatibility,  
+// however you should always compile your code on newer versions of 
+// the framework to ensure your app works the same. 
+        private static string CheckFor45DotVersion(int releaseKey)
+        {
+            if (releaseKey >= 461808)
+            {
+                return "4.7.2";
+            }
+            if (releaseKey >= 461308)
+            {
+                return "4.7.1";
+            }
+            if (releaseKey >= 460798)
+            {
+                return "4.7";
+            }
+            if (releaseKey >= 394802)
+            {
+                return "4.6.2";
+            }
+            if (releaseKey >= 394254)
+            {
+                return "4.6.1";
+            }
+            if (releaseKey >= 393295)
+            {
+                return "4.6";
+            }
+            if (releaseKey >= 393273)
+            {
+                return "4.6 RC";
+            }
+            if ((releaseKey >= 379893))
+            {
+                return "4.5.2";
+            }
+            if ((releaseKey >= 378675))
+            {
+                return "4.5.1";
+            }
+            if ((releaseKey >= 378389))
+            {
+                return "4.5";
+            }
+            // This line should never execute. A non-null release key should mean 
+            // that 4.5 is installed. 
+            return "No 4.5 version detected";
         }
 
         private void LoadSettings()
